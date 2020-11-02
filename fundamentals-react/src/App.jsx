8897728b27426e67703d085794e42bb412e753d2
Juliana Bernardes Pereira <julianabp96@gmail.com>
2020-11-02 17:01:48 +0000

    react fundamentals

--- fundamentals-react/src/App.jsx
+++ fundamentals-react/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './App.css'
 
+import Family from './components/basics/Family'
+import FamilyMember from './components/basics/FamilyMember'
 import Card from './components/layout/Card'
 import First from './components/basics/First'
 import WithParameter from './components/basics/WithParameter'
@@ -14,10 +16,18 @@ export default function App(props) {
 
             <div className="Cards">
 
+                <Card title="#05 - Component with children" color="#00C8F8">
+                    <Family surname="Pereira">
+                        <FamilyMember name="Pedro"/>
+                        <FamilyMember name="Ana"/>
+                        <FamilyMember name="Gustavo"/>
+                    </Family>
+                </Card>
+
                 <Card title="#04 - Random challenge" color="#FA6900">
                     <Random min={10} max={60} />
                 </Card>
-                
+
                 <Card title="#03 -Fragment" color="#E94C6F">
                     <Fragments />
                 </Card>
