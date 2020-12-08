65a728a152d24be8bd3637d55c3848c76af890fa
Juliana Bernardes Pereira <julianabp96@gmail.com>
2020-12-08 21:38:31 +0000

    card component

--- simple-redux/src/App.js
+++ simple-redux/src/App.js
@@ -7,10 +7,12 @@ function App() {
     <div className="App">
       <h1>React-Redux exercise (simple)</h1>
       <div className="line">
-        <Card></Card>
+        <Card title="Card 1" red>x</Card>
+        <Card title="Card 2" green>x</Card>
       </div>
       <div className="line">
-        <Card></Card>
+        <Card title="Card 3" blue>y</Card>
+        <Card title="Card 4" purple>y</Card>
       </div>
     </div>
   );
